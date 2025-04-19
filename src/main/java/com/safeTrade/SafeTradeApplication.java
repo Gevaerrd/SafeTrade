1a31c842753fd151b8d9900e70ef80e2f912cb26
Gevaerrd <gevaerrd@hotmail.com>
2025-04-19 17:37:46 -0300

    Construção do header

---
+++ src/main/java/com/safeTrade/SafeTradeApplication.java
@@ -0,0 +1,13 @@
+package com.safeTrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeTradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafeTradeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/safeTrade/safeTrade/SafeTradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safeTrade.safeTrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafeTradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
